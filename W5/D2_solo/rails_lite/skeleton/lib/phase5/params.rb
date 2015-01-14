0e5827bf30f686413eb0404c3861a1fd72ebd827
Pash DeVore <pashdevore@gmail.com>
2015-01-14 11:31:33 -0800

    working params for rails lite

--- W5/D2_solo/rails_lite/skeleton/lib/phase5/params.rb
+++ W5/D2_solo/rails_lite/skeleton/lib/phase5/params.rb
@@ -10,7 +10,26 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
-      @params = parse_www_encoded_form(req.query_string)
+      @params = {}
+
+
+      unless req.query_string.nil?
+        parse_www_encoded_form(req.query_string).each do |key, value|
+          @params[key] = value
+        end
+      end
+
+      unless req.body.nil?
+        parse_www_encoded_form(req.body).each do |key, value|
+          @params[key] = value
+        end
+      end
+
+      unless route_params.nil?
+        route_params.each do |key, value|
+          @params[key] = value
+        end
+      end
     end
 
     def [](key)
@@ -21,7 +40,7 @@ module Phase5
       @params.to_json.to_s
     end
 
-    class AttributeNotFoundError < ArgumentError; end;
+    # class AttributeNotFoundError < ArgumentError; end;
 
     private
     # this should return deeply nested hash
@@ -30,23 +49,35 @@ module Phase5
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
-      arr = URI.decode_www_form(www_encoded_form)
-      # currently turn key=val => [["key", "val"]]
-      # we want { "key" => "val"}
-      hash = {}
-      unless arr.nil?
-        arr.each do |key, value|
-          hash[key] = value
+      params = {}
+      key_value_chunks = URI.decode_www_form(www_encoded_form)
+
+      # currently key for user[addr][street]=main => [["user[address][street]", "main"]]
+      # we want {"user" => { "addr" => { "street" => "encanto" } } }
+
+      key_value_chunks.each do |key_blob, value|
+        current_node = params
+
+        # [["user[address][street]", "main"]] => ["user", "address", "street"]
+        key_array = parse_key(key_blob)
+
+        key_array.each_with_index do |key, index|
+          if index == key_array.count - 1
+            current_node[key] = value
+          else
+            current_node[key] ||= {}
+            current_node = current_node[key]
+          end
         end
       end
 
-      hash
+      params
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
-
+      key.split(/\[|\]\[|\]/)
     end
   end
 end
