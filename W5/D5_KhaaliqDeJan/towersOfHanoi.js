71c84684959c825d2ee51a68a27dda62724424f1
Pash DeVore <pashdevore@gmail.com>
2015-01-19 08:54:25 -0800

    update towers of hanoi

--- W5/D5_KhaaliqDeJan/towersOfHanoi.js
+++ W5/D5_KhaaliqDeJan/towersOfHanoi.js
@@ -1,108 +1,73 @@
-var readline = require("readline");
+function TowersOfHanoi() {
+  this.towers = [[3,2,1], [], []];
+}
 
-var reader = readline.createInterface( process.stdin,
-  process.stdout,
-  null);
-
-  function TowersOfHanoi() {
-    this.stacks = this.createBoard();
+TowersOfHanoi.prototype.isWon = function() {
+  if(this.towers[1].length == 3 || this.towers[2].length === 3) {
+    return true;
+  } else {
+    return false;
   }
+};
 
-  TowersOfHanoi.prototype.getBoardSize = function() {
-    var size = 0;
-    for(var i = 0; i < this.stacks.length; i++) {
-      size += stacks[i].length;
-    }
+TowersOfHanoi.prototype.print = function () {
+  console.log(JSON.stringify(this.towers));
+};
 
-    return size;
+TowersOfHanoi.prototype.isValidMove = function(startTowerIdx, endTowerIdx) {
+  var startTower = this.towers[startTowerIdx];
+  var endTower = this.towers[endTowerIdx];
+  console.log(startTower[startTower.length - 1]);
+  console.log(endTower[endTower.length - 1]);
+
+  if(startTower.length === 0) {
+    return false;
+  } else if(endTower.length === 0) {
+    return true;
+  } else {
+    return startTower[startTower.length - 1] < endTower[endTower.length - 1];
   }
+};
 
-  TowersOfHanoi.prototype.createBoard = function() {
-    arr = [];
-
-    for (var i = 0; i < 3; i++) {
-      arr.push([]);
-    }
-
-    return arr;
+TowersOfHanoi.prototype.move = function(startTowerIdx, endTowerIdx) {
+  if(this.isValidMove(startTowerIdx, endTowerIdx)) {
+    this.towers[endTowerIdx].push(this.towers[startTowerIdx].pop());
+    // return true;
   }
+};
 
-  TowersOfHanoi.prototype.getSizeFromUser = function (callback) {
-    var that = this;
-
-
-  }
-
-  TowersOfHanoi.prototype.isWon = function() {
-    console.log(this.boardSize);
-    console.log(this.stacks[0].length)
-    console.log(this.stacks[2].length);
-    console.log(this.stacks[1].length);
-    if((this.stacks[2].length == this.boardSize) || (this.stacks[1].length == this.boardSize)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  TowersOfHanoi.prototype.isValidMove = function (startTowerIdx, endTowerIdx) {
-    if (this.stacks[startTowerIdx].length !== 0) {
-      if (this.stacks[endTowerIdx].length == 0) {
-        return true;
-      } else if (this.stacks[endTowerIdx][-1] > this.stacks[startTowerIdx][-1]) {
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      return false;
-    }
-  }
-
-  TowersOfHanoi.prototype.move = function (startTowerIdx, endTowerIdx) {
-    try {
-      if (!this.isValidMove(startTowerIdx, endTowerIdx)) {
-        throw "This is not a valid move!";
-      } else {
-        this.stacks[endTowerIdx].push(this.stacks[startTowerIdx].pop());
-      }
-    } catch(err) {
-      console.log(err);
-    }
-  }
-
-  TowersOfHanoi.prototype.print = function () {
-    JSON.stringify(this.stacks);
-  }
-
-
-
-  TowersOfHanoi.prototype.run = function(completionCallback) {
-
-    var that = this;
-
-    reader.question("How many pieces do you want to play with?", function(size){
-      for(var i = parseInt(size); i > 0; i--) {
-        that.stacks[0].push(i);
-      }
-      console.log(that);
-
-      TowersOfHanoi.prototype.promptMove = function(callback) {
-        this.print;
-        reader.question("Where do you want to move from?", function(startIdx) {
-          reader.question("Where do you want to move to?", function(endIdx) {
-            console.log(that.isWon());
-            if(!that.isWon()) {
-              that.move(startIdx, endIdx);
-              console.log(startIdx);
-              console.log(endIdx);
-            }
-          });
-        });
-      }
-      that.promptMove();
+TowersOfHanoi.prototype.promptMove = function(reader, callback) {
+  this.print();
+  reader.question("Where would you like to move from?", function(start) {
+    var startTowerIdx = parseInt(start);
+    reader.question("Where would you like to move to?", function(end) {
+      var endTowerIdx = parseInt(end);
+      callback(startTowerIdx, endTowerIdx);
     });
-  }
+  });
+};
 
-    toh = new TowersOfHanoi();
-    toh.run();
+TowersOfHanoi.prototype.run = function(reader, completionCallback) {
+  this.promptMove(reader, (function (startTowerIdx, endTowerIdx) {
+    this.move(startTowerIdx, endTowerIdx);
+
+    if (!this.isWon()) {
+      this.run(reader, completionCallback);
+    } else {
+      this.print();
+      console.log("You win!");
+      completionCallback();
+    }
+  }).bind(this));
+};
+
+var readline = require('readline');
+var reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+var toh = new TowersOfHanoi();
+toh.run(reader, function(){
+  reader.close();
+});
